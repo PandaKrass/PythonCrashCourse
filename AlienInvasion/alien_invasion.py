87fb0826ef1c022ac19fe4e18d49e76bda22c94c
Oleh Krasovskyi <krosspanda@gmail.com>
2021-11-30 11:12:49 +0200

    Added bullets

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -12,19 +13,28 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
+            self.update_bullets()
             self._update_screen()
+
+    def update_bullets(self):
+        """Fire the bullets. Delete the bullet when it reaches top of the screen"""
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_events(self):
         # Watch for keyboard/mouse events
@@ -45,6 +55,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_ESCAPE:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def check_key_up_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -56,8 +68,16 @@ class AlienInvasion:
         # Redraw the screen in the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the last drawn screen visible
         pygame.display.flip()
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the group"""
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 if __name__ == '__main__':
